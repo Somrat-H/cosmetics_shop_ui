4d07dfcb3491743003e310dc813333a6369098b3
Hasibul Hasan Tushar <tushar.cu.cse@gmail.com>
2023-03-21 14:32:21 +0600

    server updated

---
+++ lib/server.dart
@@ -0,0 +1,27 @@
+
+
+List<String> model=[
+  "assets/model1.jpg",
+  "assets/model2.jpg",
+  "assets/model3.jpg",
+  "assets/model4.jpg",
+  "assets/model5.jpg",
+];
+List<String> foundation=[
+  "assets/foundation1.jpg",
+  "assets/foundation2.jpg",
+  "assets/foundation3.jpg",
+];
+List<String> lipstick=[
+  "assets/lipstic1.jpg",
+  "assets/lipstic2.jpg",
+  "assets/lipstic3.jpg",
+];
+List<String> eyeshadow=[
+  "assets/eyeshadow1.jpg",
+  "assets/eyeshadow2.jpg",
+];
+List<String> maskara=[
+  "assets/maskara1.jpg",
+  "assets/maskara2.jpg",
+];
